6cb8acf7359739aa20a7a0f73fce2a520f7c818b
MargaritaForster <42101996+MargaritaForster@users.noreply.github.com>
2018-08-06 00:17:35 -0400

    Update loss-of-significance.py

--- loss-of-significance.py
+++ loss-of-significance.py
@@ -1,29 +1,35 @@
 import numpy as np
 import math
+from numpy import *
+from matplotlib import pyplot
 
 #SACAR RAÍZ CUADRADA DE UN NUMERO
-numero = raw_input("Ingrese un numero: ")
-np.float32(numero)
-print "Float32: "
-print np.float32(numero)
-np.float64(numero)
-print "Float64: "
-print np.float64(numero)
 
-if numero < 0:
-    print "error"
-elif numero >= 0 :
-    a = math.sqrt(np.float32(numero))
-    b = math.sqrt(np.float64(numero))
-    print "Raíz float32"
-    print a
-    print "Raíz float64"
-    print b
-    if a > b:
-        c = ((a-b)/b)*100 
-        print "Diferencia porcentual entre float32 y float64"
-        print c
-    elif a < b:
-        d = ((b-a)/b)*100
-        print "Diferencia porcentual entre float64 y float32"
-        print d
+numero = raw_input("Ingrese un numero: ")
+float32 = np.float32(numero)
+float64 = np.float64(numero)
+lista32 = []
+lista64 = []
+lista32.append(float32)
+lista64.append(float64)
+#print lista32
+#print lista64
+
+a32 = array(lista32)
+a64 = array(lista64)
+#print a32
+#print a64
+    
+raiz32 = sqrt(a32)
+raiz64 = sqrt(a64)
+#print raiz32
+#print raiz64
+    
+i = 0
+listaError = []
+error = raiz64[i]-raiz32[i]
+listaError.append(error)
+#print listaError
+    
+print "Alt_raiz32: ", raiz32[i], "Alt_raiz64: ", raiz64[i], "Error: ", listaError[i]
+
